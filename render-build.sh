2811df34ba02ed821b3bb13150392ef9f24a6974
Dedeepya <151747427+dedeepya04@users.noreply.github.com>
2025-01-19 23:04:06 +0530

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-apt-get update
-apt-get install -y wget curl
-apt-get install -y libx11-dev libxkbfile-dev libgdk-pixbuf2.0-0 libpangocairo-1.0-0 libatk1.0-0 libcups2 libdbus-1-3 libnspr4 libnss3 libxss1 libgconf-2-4
-apt-get install -y google-chrome-stable
-
